174efda7603d0f3c28fc2ee5f0e161431636e788
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-04 21:12:25 +0600

    nodemon added

---
+++ 07_npm_intro/main.js
@@ -0,0 +1,15 @@
+// npm => node package manager
+
+// install npm packages
+
+// npm install node js packages
+// npm install express
+// npm unstall express
+
+// for existing just use npm install, it will install all the required node package
+
+
+// dev dependency
+// npm install nodemon --save-dev
+// npm install nodemon --global
+// nodemon filename

353b0871628f2e53832a0075a956ca188bba2a87
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-04 15:53:36 +0600

    main Function added

---
+++ 06_modules.js/06_main.js
@@ -0,0 +1,10 @@
+const mod = require("./module");
+let arr = [12,15,25,19]
+// console.log(mod(arr));
+
+console.log(mod.sum(arr))
+console.log(mod.avg(arr))
+console.log(mod.myName)
+console.log("This is main") 
+
+console.log(mod.nodeJs);

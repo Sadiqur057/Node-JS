4de650b6e9ac6564373c2814b73ad3a1faadf651
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-04 21:17:25 +0600

    nodemon added

--- 07_npm_intro/main.js
+++ 07_npm_intro/main.js
@@ -5,6 +5,7 @@
 // npm install node js packages
 // npm install express
 // npm unstall express
+// npm i express
 
 // for existing just use npm install, it will install all the required node package
 
@@ -13,3 +14,9 @@
 // npm install nodemon --save-dev
 // npm install nodemon --global
 // nodemon filename
+
+// dependencies : 
+// ^1.0.1 => exactly this version will be installed
+// ~1.0.1 => version near or updated can be installed
+// "express": "~4.18.2"
+// "nodemon": ">3.0.1"

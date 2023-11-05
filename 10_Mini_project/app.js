5b653b01b44965b92d8b6126b335f40f56c0d320
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-05 21:57:15 +0600

    initialized

--- 10_Mini_project/app.js
+++ 10_Mini_project/app.js
@@ -5,13 +5,14 @@ const app = express();
 const port = 80;
 
 // EXPRESS SPECIFIC CONFIG
-app.use('/static',express.static('static')); // for serving static files'
-app.use(express.urlencoded());
+app.use('/static',express.static('public')); // for serving static files'
+// EXPRESS SPECIFIC CONFIG
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded());
 
 // PUG SPECIFIC CONFIG
 app.set('view engine','pug'); // set the template engine as pug
 app.set('views', path.join(__dirname,'views')) // set the views directory
-
 // ENDPOINTS
 app.get('/',(req,res)=>{
     const params= "";

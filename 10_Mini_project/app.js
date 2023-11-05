761a0875027aa0eab88906c320856fbfdc571314
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-05 20:57:31 +0600

    initialized project

---
+++ 10_Mini_project/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC CONFIG
+app.use('/static',express.static('static')); // for serving static files'
+app.use(express.urlencoded());
+
+// PUG SPECIFIC CONFIG
+app.set('view engine','pug'); // set the template engine as pug
+app.set('views', path.join(__dirname,'views')) // set the views directory
+
+// ENDPOINTS
+app.get('/',(req,res)=>{
+    const params= "";
+})
+
+// start the server
+app.listen(port,()=>{
+    console.log(`The application is running at port ${port}`)
+})

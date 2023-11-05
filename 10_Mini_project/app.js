ed0e52678af2497b45bc450b4f5cc8fcfc3edf1e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-05 22:52:18 +0600

    corrected

--- 10_Mini_project/app.js
+++ 10_Mini_project/app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+fs = require("fs");
 const path = require('path');
 const app = express();
-const port = 80;
+port = 80;
+
 
 // EXPRESS SPECIFIC CONFIG
-app.use('/static',express.static('public')); // for serving static files'
-// EXPRESS SPECIFIC CONFIG
-// app.use('/static', express.static(path.join(__dirname, 'public')));
-// app.use(express.urlencoded());
+app.use('/public',express.static('public'));  // for using static file
+app.use(express.urlencoded());
 
 // PUG SPECIFIC CONFIG
-app.set('view engine','pug'); // set the template engine as pug
-app.set('views', path.join(__dirname,'views')) // set the views directory
+app.set('view engine', 'pug'); // set the template engine as pug
+app.set('views', path.join(__dirname, 'views'));  // set the views directory
+
 // ENDPOINTS
-app.get('/',(req,res)=>{
-    const params= "";
+app.get('/', (req, res) => {
+    res.status(200).render('index.pug')
+
+});
+
+
+app.listen(port,()=>{
+    console.log(`The application started successfully at ${port}`)
 })
 
-// start the server
-app.listen(port,()=>{
-    console.log(`The application is running at port ${port}`)
-})

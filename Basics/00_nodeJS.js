6df84c359e29c73e2dc86dd706aaad8001dfd15d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-09 20:38:18 +0600

    nodejs basics

---
+++ Basics/00_nodeJS.js
@@ -0,0 +1,8 @@
+// Node JS => A server side environment , programming environment that can be used for building high scale applications that need to support multiple concurrent request. Run on server side
+
+// modules => basic feature of functionality of programming language
+// i) core modules => const fs = require('fs') => need to import
+// ii) global modules => nodemon, globally with the system
+
+// we can import a single function from any module
+const fs = requrie('fs').writeFileSync // can be used like accessing property
